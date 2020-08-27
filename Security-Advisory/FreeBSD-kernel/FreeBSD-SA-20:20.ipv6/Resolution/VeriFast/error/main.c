913fffa1010105bea96f66bf956a7458fdd45fd7
Kiwamu Okabe <kiwamu@debian.or.jp>
2020-08-27 13:28:34 +0900

    Progress

--- Security-Advisory/FreeBSD-kernel/FreeBSD-SA-20:20.ipv6/Resolution/VeriFast/error/main.c
+++ Security-Advisory/FreeBSD-kernel/FreeBSD-SA-20:20.ipv6/Resolution/VeriFast/error/main.c
@@ -10,6 +10,8 @@ struct inpcb {
     struct mutex *mutex; // Instead of `struct rwlock`
 };
 
+//@ predicate_ctor inpcb(struct inpcb *inpcb)() = inpcb->in6p_outputopts |-> ?p &*& p->ip6po_hlim |-> _;
+
 int ip6_pcbopts(struct ip6_pktopts **pktopt)
     //@ requires pointer(pktopt, ?p) &*& ip6_pktopts_ip6po_hlim(p, _);
     //@ ensures pointer(pktopt, ?p2) &*& ip6_pktopts_ip6po_hlim(p2, 1);
@@ -20,8 +22,8 @@ int ip6_pcbopts(struct ip6_pktopts **pktopt)
 
 int
 ip6_ctloutput(struct inpcb *inp)
-    //@ requires inpcb_in6p_outputopts(inp, ?p) &*& ip6_pktopts_ip6po_hlim(p, _);
-    //@ ensures inpcb_in6p_outputopts(inp, ?p2) &*& ip6_pktopts_ip6po_hlim(p2, _);
+    //@ requires inp->in6p_outputopts |-> ?p &*& ip6_pktopts_ip6po_hlim(p, _);
+    //@ ensures inp->in6p_outputopts |-> ?p2 &*& ip6_pktopts_ip6po_hlim(p2, _);
 {
     int error;
     error = ip6_pcbopts(&inp->in6p_outputopts);
@@ -30,7 +32,21 @@ ip6_ctloutput(struct inpcb *inp)
 
 int main()
     //@ requires true;
-    //@ ensures true;
+    //@ ensures false;
 {
+    struct ip6_pktopts *opt = malloc(sizeof(struct ip6_pktopts));
+    if (opt == 0) abort();
+    struct inpcb *inp = malloc(sizeof(struct inpcb));
+    if (inp == 0) abort();
+    inp->in6p_outputopts = opt;
+    //@ close inpcb(inp)();
+    //@ close create_mutex_ghost_arg(inpcb(inp));
+    struct mutex *mutex = create_mutex();
+    inp->mutex = mutex;
+
+    while (true)
+        //@ invariant true;
+    {}
+
     return 0;
 }
