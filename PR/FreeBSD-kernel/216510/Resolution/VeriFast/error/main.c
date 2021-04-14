37d162cccdb66f73ecb9c5e2e10981cc60979bcf
Kiwamu Okabe <kiwamu@debian.or.jp>
2021-04-14 10:59:38 +0900

    Try to solve 216510 with VeriFast

---
+++ PR/FreeBSD-kernel/216510/Resolution/VeriFast/error/main.c
@@ -0,0 +1,9 @@
+#include "stdlib.h"
+#include "threading.h"
+
+int main()
+    //@ requires true;
+    //@ ensures true;
+{
+    return 0;
+}
---
+++ PR/FreeBSD-kernel/216510/Resolution/VeriFast/fix/main.c
@@ -0,0 +1,50 @@
+#include "stdlib.h"
+#include "threading.h"
+
+struct bridge_softc {
+    struct mutex *mutex;
+};
+
+//@ predicate_ctor bridge_softc(struct bridge_softc *bridge_softc)() = true;
+
+/*@
+predicate_family_instance thread_run_data(bridge_thread)(struct bridge_softc *bridge_softc) =
+    [1/100]bridge_softc->mutex |-> ?mutex &*& [1/100]mutex(mutex, bridge_softc(bridge_softc));
+@*/
+
+int bridge_delete_member(struct bridge_softc *bridge_softc)
+    //@ requires [1/100]bridge_softc->mutex |-> ?mutex;
+    //@ ensures [1/100]bridge_softc->mutex |-> _;
+{
+   return 0;
+}
+
+void bridge_clone_destroy(struct bridge_softc *bridge_softc)
+    //@ requires thread_run_data(bridge_thread)(bridge_softc);
+    //@ ensures thread_run_data(bridge_thread)(bridge_softc);
+{
+    //@ open thread_run_data(bridge_thread)(bridge_softc);
+    struct mutex *m = bridge_softc->mutex;
+    mutex_acquire(m);
+    bridge_delete_member(bridge_softc);
+    mutex_release(m);
+    //@ close thread_run_data(bridge_thread)(bridge_softc);
+}
+
+void bridge_thread(struct bridge_softc *bridge_softc) //@ : thread_run
+    //@ requires thread_run_data(bridge_thread)(bridge_softc);
+    //@ ensures false;
+{
+    while (true)
+        //@ invariant thread_run_data(bridge_thread)(bridge_softc);
+    {
+        bridge_clone_destroy(bridge_softc);
+    }
+}
+
+int main()
+    //@ requires true;
+    //@ ensures true;
+{
+    return 0;
+}
